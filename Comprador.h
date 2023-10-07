bd2a45c1af71b404f8b755bac37815f76240b186
Steph Sandoval <stephsandovalc@gmail.com>
2023-10-07 16:46:32 -0600

    clase comprador

--- Comprador.h
+++ Comprador.h
@@ -9,6 +9,8 @@
 
 #include "Material.h"
 
+using json = nlohmann::json;
+
 using namespace std;
 
 class Comprador
@@ -17,9 +19,18 @@ private:
     int minCompra, maxCompra, duracionMin, duracionMax;
 
 public:
-    Comprador(vector <int> pConfig);
+    Comprador(json pConfig){
+        minCompra = pConfig["Cantidades"]["minimo"];
+        maxCompra = pConfig["Cantidades"]["maximo"];
+        duracionMin = pConfig["Duracion"]["minimo"];
+        duracionMax = pConfig["Duracion"]["maximo"];
+    }
 
-    Material comprar(string pTipo); // hilo, parametro es el tipo de material que quiere comprar, retorna un paquete del material comprado
+    Material* comprar(string pTipo){
+        int compra = (rand() % (maxCompra - minCompra + 1)) + minCompra;
+        Material *materialComprado = new Material (pTipo, compra);
+        return materialComprado;
+    } // hilo, parametro es el tipo de material que quiere comprar, retorna un paquete del material comprado
 };
 
 #endif
