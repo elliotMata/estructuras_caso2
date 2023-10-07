53ce9194cc4163ced182c0f8e2d5b7aca9c41562
Steph Sandoval <stephsandovalc@gmail.com>
2023-10-07 17:16:06 -0600

    definiciones de node y list

--- Node.h
+++ Node.h
@@ -2,21 +2,42 @@
 #define NODE_H
 
 template <typename T>
-class Node
-{
-private:
-    T *data;
-    Node *prev;
-    Node *next;
+class Node {
+    private:
+        T *data;
+        Node *prev;
+        Node *next;
 
-public:
-    Node(T *pData = nullptr) : data(pData) {}
-    T *getData();
-    void setData(T *pData);
-    Node *getNext();
-    void setNext(Node *pNext);
-    Node getPrev();
-    void setPrev(Node *pPrev);
+    public:
+        Node(T *pData) {
+            this->data = pData;
+            next = NULL;
+            previous = NULL;
+        }
+
+        T *getData() {
+            return data;
+        }
+
+        void setData(T *pData) {
+            this->data = pData;
+        }
+
+        Node *getNext() {
+            return next;
+        }
+
+        Node *getPrevious() {
+            return previous;
+        }
+
+        void setNext(Node *pValue) {
+            this->next = pValue;
+        }
+
+        void setPrevious(Node *pValue) {
+            this->previous = pValue;
+        }
 };
 
 #endif
